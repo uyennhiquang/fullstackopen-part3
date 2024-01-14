125046702aa8ba7ba5a34f21198b97c333e1fa37
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-01-14 15:29:31 -0500

    push

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan")
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -79,7 +81,7 @@ app.post("/api/persons", (request, response) => {
   response.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });
